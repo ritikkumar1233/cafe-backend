4dcda678599f71b32cc39524c8eef4a1327ef84c
ritikkumar1233 <ritikroushan96905@gmail.com>
2025-07-08 15:25:55 +0530

    first commit

---
+++ models/userModel.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+const userSchema = mongoose.Schema(
+  {
+    username: { type: String },
+    email: { type: String, unique: true },
+    password: { type: String },
+    role: { type: String, default: "user" },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
---
+++ server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/userRoute.js";
+
+dotenv.config();
+const app = express();
+
+const dbuser = encodeURIComponent(process.env.DBUSER);
+const dbpass = encodeURIComponent(process.env.DBPASS);
+
+
+app.use(express.json());
+mongoose.connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.7rykw6s.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`).then(() => {
+  app.listen(8080, () => {
+    console.log("Server started");
+  });
+});
+
+app.use("/api/users", userRouter);
+
+
+
+// app.use(express.json());
+// mongoose.connect(`mongodb://localhost:27017/merncafe`).then(() => {
+//   app.listen(8080, () => {
+//     console.log("Server started");
+//   });
+// });
+
+
+// app.use(express.json());
+// mongoose.connect(`mongodb://${dbpass}:${dbpass}@localhost:27017/merncafe`).then(() => {
+//   app.listen(8080, () => {
+//     console.log("Server started");
+//   });
+// });
